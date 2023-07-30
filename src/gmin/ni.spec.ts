e9cf235e6be3dd51d95d638513c93dcd3e4b3440
zih0 <ziho@kakao.com>
2023-07-31 00:55:47 +0900

    Revert "fix: ci"
    This reverts commit 2db6f9cc61a536d611b16bf89159b56c9a1f97e1.

--- src/gmin/ni.spec.ts
+++ src/gmin/ni.spec.ts
@@ -1,34 +1,29 @@
-import { describe, expect, test } from 'vitest';
-import { getCommand } from '../sampleCode/C06/parse';
-import { AGENTS } from '../sampleCode/C06/agents';
+import {describe, expect, test} from "vitest";
+import {getCommand} from "../sampleCode/C06/parse";
+import {AGENTS} from "../sampleCode/C06/agents";
 
 //given when then
 
-describe('getCommand 테스트', () => {
-  test('지원하지 않는 agent 입력 시 Unsupported agent "${agent}" 에러가 출력되는가?', () => {
-    const agent = 'gmin';
-    const command = 'install';
-    expect(() =>
-      getCommand(agent, command).toThrowError(`Unsupported agent "${agent}"`)
-    );
+describe('getCommand 테스트', ()=>{
+    test('지원하지 않는 agent 입력 시 Unsupported agent "${agent}" 에러가 출력되는가?', ()=>{
+     const agent = 'gmin'
+    const command = 'install'
+    expect(()=> getCommand(agent, command).toThrowError(`Unsupported agent "${agent}"`))
   }),
     test('지원하지 않는 command 입력 시 `Command "${command}" is not support by agent "${agent}"` 에러가 출력 되는가?', () => {
       const agent = Object.keys(AGENTS)[0]; // 가장 첫번째 agent를 가져옵니다.
       const command = 'gmin';
-      expect(() =>
-        getCommand(agent, command).toThrowError(
-          `Command "${command}" is not support by agent "${agent}"`
-        )
-      );
+      expect(()=> getCommand(agent, command).toThrowError(`Command "${command}" is not support by agent "${agent}"`))
     });
-  test.skip('함수형 외의 명령어 입력 시 명령어 포맷이 잘 되는가?', () => {
-    const agent = 'agent';
-    const command = 'command';
-    const args = ['arg1', 'arg2', 'arg3'];
+    test('함수형 외의 명령어 입력 시 명령어 포맷이 잘 되는가?', () => {
+      const agent = 'agent';
+      const command = 'command';
+      const args = ['arg1', 'arg2', 'arg3'];
 
-    AGENTS[agent] = { [command]: 'test formatting command {0}' };
-    const result = getCommand(agent, command, args);
+      AGENTS[agent] = {[command] : 'test formatting command {0}'}
+      const result = getCommand(agent, command, args);
 
-    expect(result).stringMatching('test formatting command ' + args.join(' '));
-  });
-});
+      expect(result).stringMatching('test formatting command ' + args.join(' '));
+    });
+})
+
