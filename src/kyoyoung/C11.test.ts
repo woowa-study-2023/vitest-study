82940e6761127a414c9896791b2e182cfdcdc3a5
keumky2 <keumky2@woowahan.com>
2023-08-10 09:55:29 +0900

    코드 정리

--- src/kyoyoung/C11.test.ts
+++ src/kyoyoung/C11.test.ts
@@ -11,14 +11,6 @@ const api = supertest(app)
 let mongod: MongoMemoryServer;
 
 
-
-// For mongodb-memory-server's old version (< 7) use this instead:
-// const mongoServer = new MongoMemoryServer();
-
-const opts = {
-  useUnifiedTopology: true,
-};
-
 // Provide connection to a new in-memory database server.
 const connect = async () => {
   // NOTE: before establishing a new connection close previous
@@ -67,123 +59,6 @@ afterEach(async()=>{
   await clear()
 })
 
-describe('when there is initially some notes saved', () => {
-
-
-  test('notes are returned as json', async () => {
-    await api
-      .get('/api/notes')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-  })
-
-  test('all notes are returned', async () => {
-    const response = await api.get('/api/notes')
-
-    expect(response.body).toHaveLength(helper.initialNotes.length)
-  })
-
-  test('a specific note is within the returned notes', async () => {
-    const response = await api.get('/api/notes')
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const contents = response.body.map((r:any) => r.content)
-    expect(contents).toContain(
-      'Browser can execute only JavaScript'
-    )
-  })
-
-  describe('viewing a specific note', () => {
-
-    test('succeeds with a valid id', async () => {
-      const notesAtStart = await helper.notesInDb()
-      const noteToView = notesAtStart[0]
-
-      const resultNote = await api
-        .get(`/api/notes/${(noteToView as any).id}`)
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-
-      expect(resultNote.body).toEqual(noteToView)
-    })
-
-    test('fails with statuscode 404 if note does not exist', async () => {
-      const validNonexistingId = await helper.nonExistingId()
-
-
-      await api
-        .get(`/api/notes/${validNonexistingId}`)
-        .expect(404)
-    })
-
-
-    test('fails with statuscode 400 id is invalid', async () => {
-      const invalidId = '5a3d5da59070081a82a3445'
-
-      await api
-        .get(`/api/notes/${invalidId}`)
-        .expect(400)
-    })
-  })
-
-  describe('addition of a new note', () => {
-    test('succeeds with valid data', async () => {
-      const newNote = {
-        content: 'async/await simplifies making async calls',
-        important: true,
-      }
-
-      await api
-        .post('/api/notes')
-        .send(newNote)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-      const notesAtEnd = await helper.notesInDb()
-      expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1)
-
-      const contents = notesAtEnd.map(n => n.content)
-      expect(contents).toContain(
-        'async/await simplifies making async calls'
-      )
-    })
-
-    test('fails with status code 400 if data invalid', async () => {
-      const newNote = {
-        important: true
-      }
-
-      await api
-        .post('/api/notes')
-        .send(newNote)
-        .expect(400)
-
-      const notesAtEnd = await helper.notesInDb()
-
-      expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
-    })
-  })
-
-  describe('deletion of a note', () => {
-    test('succeeds with status code 204 if id is valid', async () => {
-      const notesAtStart = await helper.notesInDb()
-      const noteToDelete = notesAtStart[0]
-
-      await api
-        .delete(`/api/notes/${(noteToDelete as any).id}`)
-        .expect(204)
-     const notesAtEnd = await helper.notesInDb()
-
-      expect(notesAtEnd).toHaveLength(
-        helper.initialNotes.length - 1
-      )
-
-      const contents = notesAtEnd.map(r => r.content)
-
-      expect(contents).not.toContain(noteToDelete.content)
-    })
-  })
-})
 
 afterAll(async () => {
 await close()
